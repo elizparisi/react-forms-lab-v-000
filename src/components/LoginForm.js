bf15395035a0f67f892cd07e68d67192aa3560cf
Liz P <parisielizabeth@gmail.com>
2021-02-02 20:43:47 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,9 +19,8 @@ class LoginForm extends React.Component {
   handleSubmit = event => {
     event.preventDefault();
     
-    if (this.state.username && this.state.password) {
-      this.props.handleLogin(this.state)
-    }
+    if (this.state.username && this.state.password)
+      return this.props.handleLogin(this.state)
   }
 
   render() {
